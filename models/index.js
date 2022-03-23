4df06340481dcb60162ebe0e4d9ee45b70e36e3c
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-22 22:37:41 -0500

    defined table and started associations

--- models/index.js
+++ models/index.js
@@ -3,14 +3,34 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { hasMany } = require('./Product');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: '',
+  foreignKey: ''
+})
+
+Tag.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
