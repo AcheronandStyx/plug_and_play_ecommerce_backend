347b604293a7a146e73033870069ff253e18fb10
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-22 21:54:02 -0500

    starter code & sequelize connection

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection'); // import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+// If we flag force to true it forces a table recreate if there are association changes
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port http://localhost:${PORT} !`);
+  });
+});
